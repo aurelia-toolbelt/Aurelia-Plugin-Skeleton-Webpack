795163958b0f45f947ccd6bbd615915bb3021706
Hamed Fathi <HamedFathi@users.noreply.github.com>
2019-03-16 20:38:42 +0330

    Update demo.e2e.ts

--- test/e2e/demo.e2e.ts
+++ test/e2e/demo.e2e.ts
@@ -7,7 +7,7 @@ const PORT = setting.build.targets[0].port;
 
 describe('My Aurelia Test', function() {
   it('get message', function() {
-    cy.visit(`http://localhost:${8080}/`);
+    cy.visit(`http://localhost:8080`);
 
     cy.get("h1").should( _tr => {
       const value = _tr.text();
